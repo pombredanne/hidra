a6d5b83fe96923a5c9c436b7b38a2d7b38e430ea
Javier de la Rosa <versae@gmail.com>
2013-10-20 14:02:36 -0400

    More attempts to get this working... with no luck

--- hidra/hidra.py
+++ hidra/hidra.py
@@ -5,9 +5,9 @@ from flask import Flask
 from flask import request
 from flask import Response
 
-from ws4py.client.geventclient import WebSocketClient
+from werkzeug import LocalProxy
 
-from sockets import websocket
+from ws4py.client.geventclient import WebSocketClient
 
 app = Flask(__name__)
 app.debug = True
@@ -19,29 +19,53 @@ PROXY_REWRITE_REGEX = re.compile(
     '\s*[=:]\s*["\']?)/',
     re.IGNORECASE
 )
+websocket = LocalProxy(lambda: request.environ.get('wsgi.websocket', None))
 websockets = {}
 
 
-@app.route('/proxy/', defaults={'url': ''}, methods=[
-    "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"
-])
-@app.route('/proxy/<path:url>', methods=[
-    "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"
-])
+class WebSocketProxy(WebSocketClient):
+    def __init__(self, to, *args, **kwargs):
+        self.to = to
+        print(("Proxy to", self.to))
+        super(WebSocketProxy, self).__init__(*args, **kwargs)
+
+    def opened(self):
+        m = self.to.receive()
+        print("<= %d %s" % (len(m), str(m)))
+        self.send(m)
+
+    def closed(self, code, reason):
+        print(("Closed down", code, reason))
+
+    def received_message(self, m):
+        print("=> %d %s" % (len(m), str(m)))
+        self.to.send(m)
+
+methods = ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH",
+           "CONNECT"]
+
+
+@app.route('/proxy/', defaults={'url': ''}, methods=methods)
+@app.route('/proxy/<path:url>', methods=methods)
 def proxy(url):
-    if websocket:
-        data = websocket.receive()
-        websocket_url = 'ws://{}/{}'.format(PROXY_DOMAIN, url)
-        if websocket_url in websockets:
-            client = websockets[websocket_url]
-        else:
-            client = WebSocketClient(websocket_url,
-                                     protocols=['http-only', 'chat'])
-            websockets[websocket_url] = client
-        client.connect()
-        client.send(data)
-        client_data = client.receive()
-        websocket.send(client_data)
+    with app.test_request_context():
+        if websocket:
+            while True:
+                data = websocket.receive()
+                websocket_url = 'ws://{}/{}'.format(PROXY_DOMAIN, url)
+                if websocket_url not in websockets:
+                    client = WebSocketClient(websocket_url,
+                                             protocols=['http-only', 'chat'])
+                    websockets[websocket_url] = client
+                else:
+                    client = websockets[websocket_url]
+                client.connect()
+                if data:
+                    client.send(data)
+                client_data = client.receive()
+                if client_data:
+                    websocket.send(client_data)
+            return Response()
     if request.method == "GET":
         url_ending = "%s?%s" % (url, request.query_string)
         url = PROXY_FORMAT % url_ending
